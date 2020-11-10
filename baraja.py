ee907bb81bfcd79e81a305f90b614b93a7ea213b
Ciprian Nica <ciprian@MacBook-Pro-de-Ciprian.local>
2020-11-11 00:27:07 +0100

    func creaBaraja

--- baraja.py
+++ baraja.py
@@ -1,7 +1,7 @@
 palos =['o', 'c', 'e', 'b']
 numeros =['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
-baraja = []
 def crearBaraja():
+baraja = []
     for numero in numeros:
         for palo in palos:
             baraja.append(numero+palo)
